5cec9a6f05f4a2a15d17f48b9ca8df4b67e40422
kakekita <117062289+kakekita@users.noreply.github.com>
2023-06-16 19:47:06 +0900

    Add files via upload

--- play/mySketch.js
+++ play/mySketch.js
@@ -1,5 +1,17 @@
+var user_name = "";
+
 function setup() {
-	
+	var ls = localStorage.getItem("user_name");
+	if(ls == null) {
+		ls = ""
+	}
+	var user_input = null
+	while(user_input == null) {
+			user_input = prompt("アカウントの数字の部分を入力（XXXXXXXXX@sch.ed.kakegawa-net.jp）",ls);
+	}
+	user_name = user_input
+	localStorage.setItem('user_name', user_name);
+	getData();
 }
 
 function draw() {
@@ -38,11 +50,17 @@ async function getData() {
 }
 
 
-function createTable(dict,ni) {
+async function createTable(dict,ni) {
 	var mybody = document.getElementById("div_" + ni);
 	var mytable = document.createElement("table");
 	mytable.classList.add("table_" + ni);
 	var mytbody = document.createElement("tbody");
+	var db = firebase.firestore();
+	var postRef1 = db.collection("favorite").doc(String(user_name));
+	var fd = null
+	await postRef1.get().then(e => {
+		fd = e.data();
+	});
 	for (var k in dict) {
 		var mytr = document.createElement("tr");
 		var myth = document.createElement("th");
@@ -54,18 +72,25 @@ function createTable(dict,ni) {
 		var mytd5 = document.createElement("td");
 		var mytd6 = document.createElement("td");
 		var mytd7 = document.createElement("td");
-		myth.textContent = String(dict[k]["date"].toDate().getMonth()+1)+"/"+String(dict[k]["date"].toDate().getDate())+" "+String(dict[k]["date"].toDate().getHours())+":"+String("0"+String(dict[k]["date"].toDate().getMinutes())).slice(-2);
-		mytd1.textContent = String(dict[k]["title"]);
-		mytd2.textContent = String(dict[k]["hint1"]);
-		mytd3.textContent = String(dict[k]["hint2"]);
-		mytd4.textContent = String(dict[k]["hint3"]);
-		mytd5.insertAdjacentHTML('beforeend','<a id=a_'+String(k)+' onclick="switch_answer('+k+',this.textContent)">表示</a>');
-		mytd6.textContent = String(dict[k]["answer"]);
-		mytd6.classList.add("hidden");
-		mytd6.id = "td1_"+String(k);
-		mytd7.textContent = String(dict[k]["explain"]);
+		var mytd8 = document.createElement("td");
+		var mytd9 = document.createElement("td");
+		myth.insertAdjacentHTML('beforeend','<a style="color: white" id=f_'+String(k)+' onclick="switch_favorite('+k+')">★</a>');
+		mytd1.textContent = String(dict[k]["date"].toDate().getMonth()+1)+"/"+String(dict[k]["date"].toDate().getDate())+" "+String(dict[k]["date"].toDate().getHours())+":"+String("0"+String(dict[k]["date"].toDate().getMinutes())).slice(-2);
+		mytd2.textContent = String(dict[k]["title"]);
+		mytd2.id = "td4_"+String(k);
+		mytd3.textContent = String(dict[k]["hint1"]);
+		mytd4.textContent = String(dict[k]["hint2"]);
+		mytd5.textContent = String(dict[k]["hint3"]);
+		mytd6.insertAdjacentHTML('beforeend','<a class="link_a" id=a_'+String(k)+' onclick="switch_answer('+k+',this.textContent)">表示</a>');
+		mytd7.textContent = String(dict[k]["answer"]);
 		mytd7.classList.add("hidden");
-		mytd7.id = "td2_"+String(k);
+		mytd7.id = "td1_"+String(k);
+		mytd8.textContent = String(dict[k]["explain"]);
+		mytd8.classList.add("hidden");
+		mytd8.id = "td2_"+String(k);
+		mytd9.textContent = String(dict[k]["account"]);
+		mytd9.id = "td3_"+String(k);
+		mytd9.style.display = "none"
 		//if(parseInt(dict[k]["value"]) < 1000) {
 		mytr.appendChild(myth);
 		mytr.appendChild(mytd1);
@@ -75,11 +100,27 @@ function createTable(dict,ni) {
 		mytr.appendChild(mytd5);
 		mytr.appendChild(mytd6);
 		mytr.appendChild(mytd7);
+		mytr.appendChild(mytd8);
+		mytr.appendChild(mytd9);
 		mytbody.appendChild(mytr);
-		//}
 	}
 	mytable.appendChild(mytbody);
 	mybody.appendChild(mytable);
+	
+	for (var k2 in dict) {
+		var elem1 = document.getElementById("f_"+String(k2))
+		var elem2 = document.getElementById("td4_"+String(k2))
+		var elem3 = document.getElementById("td3_"+String(k2))
+		console.log("fd,",fd)
+		console.log(elem3.textContent);
+		for(var d of Object.keys(fd)) {
+			if(elem3.textContent != d) continue;
+			console.log(fd[d], elem2.textContent)
+			if(fd[d].indexOf(elem2.textContent) != -1) {
+				elem1.style.color = "gold";
+			}
+		}
+	}
 }
 
 
@@ -98,4 +139,39 @@ function switch_answer(n,str) {
 	}
 }
 
-getData();
+async function switch_favorite(n) {
+	var db = firebase.firestore();
+	var elem = document.getElementById("f_"+String(n))
+	var elem2 = document.getElementById("td3_"+String(n))
+	var elem3 = document.getElementById("td4_"+String(n))
+	if(elem.style.color == "white") {
+		elem.style.color = "gold";
+		var postRef1 = db.collection("favorite").doc(String(user_name));
+		var fd = [];
+		await postRef1.get().then(e => {
+			var d = e.data();
+			if(d != null) fd = d;
+		});
+		console.log(fd)
+		fd[elem2.textContent].push(elem3.textContent);
+		save_database(elem2.textContent,fd[elem2.textContent],"favorite",String(user_name));
+	}else if(elem.style.color == "gold") {
+		elem.style.color = "white";
+		var userRef = db.collection("favorite").doc(String(user_name));
+		var fd2 = [];
+		await userRef.get().then(e => {
+			fd2 = e.data();
+			console.log(fd2[elem2.textContent])
+			fd2[elem2.textContent].splice(fd2[elem2.textContent].indexOf(elem3.textContent),1);
+			console.log(fd2[elem2.textContent])
+		});
+		save_database(elem2.textContent,fd2[elem2.textContent],"favorite",String(user_name));
+	}
+}
+
+function save_database(num,s,mw,d) {
+	var db = firebase.firestore();
+  var userRef = db.collection(mw).doc(d);
+  num = String(num)
+  userRef.set({[num]: s}, { merge: true });
+}
